df61a6ba0bd666560a4bd35f5587e2f3fdafc1b0
Evan Yan <evanyans@gmail.com>
2023-07-07 16:40:11 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `personal-website-v2`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
